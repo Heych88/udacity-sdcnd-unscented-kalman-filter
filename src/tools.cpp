01b760563aaf5eff029172996bb3e3ca2b5bd81e
Haidyn Mcleod <haidyn1988@gmail.com>
2017-06-20 13:07:42 +1000

    add RMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,27 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+  
+  // verify the input data is as expected
+  int size = estimations.size();
+  if(size != ground_truth.size() || size == 0){
+    std::cout << "ERROR: Miss-match in RMSE dimensions. estimations.size() = "
+        << size << ",  ground_truth.size() = " << ground_truth.size() 
+        << std::endl;
+    return rmse;
+  }
+  
+  // RMSE = sqrt(1/(vector size) * sum(square(estimate-truth)))
+  for(int i=0; i < size; i++){
+    VectorXd difference = estimations[i] - ground_truth[i];
+    difference = difference.array() * difference.array();
+    rmse = rmse + difference;
+  }
+  std::cout << "done" << std::endl;
+  rmse = rmse / size;
+  rmse = rmse.array().sqrt();
+  std::cout << rmse << std::endl;
+  return rmse;
 }
